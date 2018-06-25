acaaa4a0e75ab5f47a23f35651ea7215a1a7448e
QQ <q.lenhuquynh@gmail.com>
2018-06-25 15:58:06 +0300

    change the title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => (
     <div className="header">
-        <h1>Let's talk</h1>
+        <h1>Let's talk Form</h1>
         <p>Think you have what it takes? Show us.</p>
     </div>
 )
