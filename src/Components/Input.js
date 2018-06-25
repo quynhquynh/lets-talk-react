5b91b7c2dac5fd2342268596efd01a35a3dbc014
QQ <q.lenhuquynh@gmail.com>
2018-06-25 15:50:44 +0300

    Updates

--- src/Components/Input.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Input = ({name, placeholder, value, className, onBlur, onChange}) => (
-    <input type='text' name={name} value={value} className={className} onBlur={onBlur} placeholder={placeholder} onChange={onChange} />
-)
-
-export default Input
